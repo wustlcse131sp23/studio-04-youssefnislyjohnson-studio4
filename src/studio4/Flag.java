3fcc9e9c57f6c968376cae6d10af3bca27da4b86
marcusjohnson <marcusjohnson@localhost>
2023-02-23 12:06:04 -0600

    made a flag

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,6 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
-		
+		StdDraw.picture(0.5, 0.5, "src/studio4/WashU_Logo.svg.png", 0.75, 0.75, 180);
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,6 +1,8 @@
 package studio4;
 
+import java.awt.Color;
 import java.io.File;
+
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
@@ -19,6 +21,17 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
-		
+		String shape = in.next();
+		int red = in.nextInt();
+		int green = in.nextInt();
+		int blue = in.nextInt();
+		boolean isFilled = in.nextBoolean();
+		double parameterOne = in.nextDouble();
+		double parameterTwo = in.nextDouble();
+		double parameterThree = in.nextDouble();
+		double parameterFour = in.nextDouble();
+		Color color = new Color(red, green, blue);
+		StdDraw.setPenColor(color);
+		StdDraw.filledRectangle(parameterOne, parameterTwo, parameterThree, parameterFour);
 	}
 }
